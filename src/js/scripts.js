a6e539292e798b91ed75469310ad5d93bbd5ddec
LuanSouzaSilveira <luansouzasilveira@gmail.com>
2020-10-13 13:32:28 -0300

    feature: inserted charts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -30,3 +30,29 @@ langSwitcher.addEventListener('click', () => {
 		title.innerHTML = 'Olá Mundo!';
 	}
 })
+
+const ctx = document.querySelector('#myCanvas');
+const chart = new Chart(ctx , {
+	type: 'bar',
+	data: {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+	},
+	options: {
+		responsive: true
+	}
+});
